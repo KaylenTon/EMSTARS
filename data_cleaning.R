963f0e21e73e0c07bceb1375452eefd1cebc525b
KaylenTon <kaylent2k@gmail.com>
2026-02-15 20:05:43 -0500

    getting started with duckdb, trying to load the data to work with efficiently but idk

---
+++ data_cleaning.R
@@ -0,0 +1,41 @@
+library(readr)
+library(tools)
+library(duckdb)
+library(duckplyr)
+library(lubridate)
+
+# Exploring 20 sample -----------------------------------------------------
+
+path <- "~/R PRACTICE/FL_EMSTARS_csv"
+
+files <- list.files(
+  path,
+  pattern = "//.csv$",
+  full.names = T
+)
+
+system.time(
+  data <- lapply(files, read_csv, n_max = 20)
+)
+
+names(data) <- file_path_sans_ext(basename(files))
+
+
+# DuckDB attempt ----------------------------------------------------------
+
+con <- dbConnect(duckdb(), dbdir = "research_db.duckdb", read_only = FALSE)
+
+dbExecute(con, "
+CREATE TABLE patient1 AS
+SELECT 
+  PatientId::VARCHAR AS PatientId,
+  ReportsID::VARCHAR AS ReportsID,
+  Gender::DOUBLE AS Gender,
+  Age::DOUBLE AS Age
+FROM read_csv_auto(
+  'C:/Users/Kaylen/OneDrive - University of South Florida/Documents/R PRACTICE/FL_EMSTARS_csv/patient.csv',
+  nullstr='.'
+)
+")
+
+test <- dbGetQuery(con, "SELECT * FROM patient1 LIMIT 5;")
